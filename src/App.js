ac5fad20f79f1eaeb7018a53e6184d2cd1e1d199
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-08-24 14:19:48 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import MainPage from "./pages/Main";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <MainPage />
     </div>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import './navbar.css'
+
+const Navbar = () => {
+
+    // to change burger classes
+    const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+    const [menu_class, setMenuClass] = useState("menu hidden")
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+    // toggle burger menu change
+    const updateMenu = () => {
+        if (!isMenuClicked) {
+            setBurgerClass("burger-bar clicked")
+            setMenuClass("menu visible")
+        }
+        else {
+            setBurgerClass("burger-bar unclicked")
+            setMenuClass("menu hidden")
+        }
+        setIsMenuClicked(!isMenuClicked)
+    }
+
+    return (
+        <div style={{ width: '10%', height: '10vh' }}>
+            <nav>
+                <div className="burger-menu" onClick={updateMenu}>
+                    <div className={burger_class} >
+                    </div>
+                    <div className={burger_class} ></div>
+                    <div className={burger_class} ></div>
+                </div>
+                <img src="../imagenes/logoB.jpg" className="img-logo" alt="" width={"150px"} />
+            </nav>
+
+            <div className={menu_class}>
+                <h1> Obras </h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
