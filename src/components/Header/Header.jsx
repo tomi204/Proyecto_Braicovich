83eb99fee53ec52137bd2f5f9941e47a6ae35409
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-08-29 15:51:53 -0300

    header y cambio de lugar del logo

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavL from '../Lateralnav/NavL'
+import Navbar from '../Navbar/Navbar'
+
+const Header = () => {
+    <>
+        <NavL />
+        <Navbar />
+    </>
+}
+
+export default Header
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 import "./main.css"
 const MainPage = () => {
     return (
-        <section className='main-page'></section>
+        <section className='main-page'>
+            <div className='main-div'>
+
+            </div>
+        </section>
     )
 }
 
